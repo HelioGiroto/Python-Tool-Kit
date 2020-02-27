4a6c134dadf55a3678788dce9142ce59da3eaae5
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2020-02-27 13:45:52 -0300

    OPENPYXL - Sort de colunas + sumif(), sum(), countif()

--- CFC.py
+++ CFC.py
@@ -41,7 +41,7 @@ def mudaNome(nro):
 	return nro
 
 
-listaDados = [['DATA', 'NOME', 'SERVIÇO', 'VALOR']] 
+listaDados = [['DATA', 'NOME', 'SERVIÇO', 'VALOR']]
 
 def lancaDados():
 	while True:
@@ -106,7 +106,7 @@ def lancaDados():
 		else:
 			print(data)
 			listaDados.append(dadosServico)
-			os.system('clear') # opcional
+			os.system('clear')
 			# windows:
 			# os.system('cls')
 	print()
@@ -136,40 +136,63 @@ def salvaPlanilha(listaDados):
 	somaDem    = '=SUMIF(C2:C'+str(qtdeServicos)+', "DEMISSIONAL", D2:D'+str(qtdeServicos)+')'
 	celTotal   = '=SUM(D2:D'+str(qtdeServicos)+')'
 
+	qtdeExMed  = '=COUNTIF(C2:C'+str(qtdeServicos)+', "EXAME MÉDICO")'
+	qtdePsi    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "EXAME PSICOTÉCNICO")'
+	qtdeMedPsi = '=COUNTIF(C2:C'+str(qtdeServicos)+', "EXAME MÉDICO/PSICOTÉCNICO")'
+	qtdeCfc    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "CURSO CFC PH")'
+	qtdeRec    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "CURSO RECICLAGEM")'
+	qtdeRen    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "CURSO RENOVAÇÃO")'
+	qtdeSim    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "AULA SIMULADOR")'
+	qtdeAdm    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "ADMISSIONAL")'
+	qtdeDem    = '=COUNTIF(C2:C'+str(qtdeServicos)+', "DEMISSIONAL")'
+	qtdeTotal   = '=SUM(H2:H10)'
+
 	# Poderia ser se não tivesse o recurso de sortear as colunas:
 	# ws.cell(row=qtdeServicos+1, column=3).value = 'VALOR TOTAL: '
 	# ws.cell(row=qtdeServicos+1, column=4).value = celTotal
 
+	ws.cell(row=1, column=7) = 'R$'
+	ws.cell(row=1, column=8) = 'Qtde'
 
 	ws.cell(row=2, column=6).value = 'Exame Médico: '
 	ws.cell(row=2, column=7).value = somaExMed
+	ws.cell(row=2, column=8).value = qtdeExMed
 
 	ws.cell(row=3, column=6).value = 'Exame Psicotécnico: '
 	ws.cell(row=3, column=7).value = somaPsi
+	ws.cell(row=3, column=8).value = qtdePsi
 
 	ws.cell(row=4, column=6).value = 'Exame Médico/Psicotécnico: '
 	ws.cell(row=4, column=7).value = somaMedPsi
+	ws.cell(row=4, column=8).value = qtdeMedPsi
 
 	ws.cell(row=5, column=6).value = 'Curso CFC PH: '
 	ws.cell(row=5, column=7).value = somaCfc
+	ws.cell(row=5, column=8).value = qtdeCfc
 
 	ws.cell(row=6, column=6).value = 'Curso Reciclagem: '
 	ws.cell(row=6, column=7).value = somaRec
+	ws.cell(row=6, column=8).value = qtdeRec
 
 	ws.cell(row=7, column=6).value = 'Curso Renovação: '
 	ws.cell(row=7, column=7).value = somaRen
+	ws.cell(row=7, column=8).value = qtdeRen
 
 	ws.cell(row=8, column=6).value = 'Aula Simulador: '
 	ws.cell(row=8, column=7).value = somaSim
+	ws.cell(row=8, column=8).value = qtdeSim
 
 	ws.cell(row=9, column=6).value = 'Admissional: '
 	ws.cell(row=9, column=7).value = somaAdm
+	ws.cell(row=9, column=8).value = qtdeAdm
 
 	ws.cell(row=10, column=6).value = 'Demissional: '
 	ws.cell(row=10, column=7).value = somaDem
+	ws.cell(row=10, column=8).value = qtdeDem
 
 	ws.cell(row=12, column=6).value = 'VALOR TOTAL: '
 	ws.cell(row=12, column=7).value = celTotal
+	ws.cell(row=12, column=8).value = qtdeTotal
 
 	wb.save(planilha + '.xlsx')
 
@@ -205,6 +228,6 @@ lancaDados()
 # https://openpyxl.readthedocs.io/en/stable/filters.html
 # https://openpyxl.readthedocs.io/en/stable/worksheet_tables.html
 
-
+# https://banco.bradesco/assets/pessoajuridica/aplicativos/navegador-exclusivo/windows/Instalador.exe
 
 
